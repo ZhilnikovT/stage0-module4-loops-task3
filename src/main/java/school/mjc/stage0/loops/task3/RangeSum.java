9482a18b998011cf959fa6444ed1759456b6e365
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-25 16:31:44 +0300

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
+    
+    public static void main(String[] arg){
+        RangeSum r = new RangeSum();
+        r.printSumInclusive(0, 110);
+    }
+    
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        long sum = 0;
+        for(int i = firstBoarder; i <= secondBoarder; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
