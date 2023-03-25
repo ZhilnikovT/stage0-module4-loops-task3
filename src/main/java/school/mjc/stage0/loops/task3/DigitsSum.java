a373a4a36c6ab5a56379a9966a5d5751e14f083b
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-25 18:15:14 +0300

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,23 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
+    
+    public static void main(String[] arg){
+        DigitsSum r = new DigitsSum();
+        r.printDigitsSum(300);
+    }
+    
     public void printDigitsSum(int t){
-
+        if (t < 0){t = -t;}
+        int sum = 0;
+        String s = Integer.toString(t);
+        int d = 10;
+        for(int i = s.length() - 1; i > 0; i--){
+            
+            int n = t - d*(int)(t / d);
+            sum = sum + n;
+            t = t / 10;
+        }
+        System.out.println(sum + t);
     }
 }
