66aee53b7158de72b2e8ff6cc07831afdcbb4a59
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-25 16:48:37 +0300

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
+    
+    public static void main(String[] arg){
+        AlphabetPrinter r = new AlphabetPrinter();
+        r.printAlphabet();
+    }
+    
     public void printAlphabet() {
+        for(int i = 97; i <= 122; i++){
+            char c = (char) i;
+            System.out.println(c);
+        }
     }
 }
